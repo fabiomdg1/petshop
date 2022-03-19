38d5ee46b48b4a407b985af8242e90f45a06f0b0
Fabio <fabiomdg1@gmail.com>
2022-03-19 15:30:34 -0300

    Adicionando comentarios

--- frontEndPetshop/petshopFront/src/app/Componentes/Clientes/dados-do-cliente/dados-do-cliente.component.ts
+++ frontEndPetshop/petshopFront/src/app/Componentes/Clientes/dados-do-cliente/dados-do-cliente.component.ts
@@ -184,6 +184,7 @@ export class DadosDoClienteComponent implements OnInit {
       })
   }
 
+  //Utilizada no botão Buscar
   //Função utilizada para popular os campos no html com os dados do cliente que foram puxados do banco
   mostrar_dados(){
     console.log('Mostrar dados')
