8a61abe9a86a2a1f405a670735929df91316ff20
Fabio <fabiomdg1@gmail.com>
2022-03-19 20:30:31 -0300

    Add readme

---
+++ readme.md
@@ -0,0 +1,26 @@
+
+<h1 align="center">Tetty's Dog </h1>
+<h3 align="center">Site de Petshop</h3>
+
+---
+
+## SoulCode Challenge 💡
+
+Desafio de HTML5, CSS3 proposto pela <strong>Soul Code</strong>, EDTECH brasileira com <i>inclusão social.</i>
+
+- <a href="https://soulcodeacademy.org/">SoulCode</a>
+
+## Techs 🛠
+
+- HTML5
+- CSS3
+
+---
+
+## Authors 👨‍💻
+
+- GitHub: [@Fabio](https://github.com/fabiomdg1)
+
+---
+
+Copyright - 2021 Fabio Mattos, | SoulCode Challenge
--- src/main/java/petshop/td/cliente/repository/ClienteRepository.java
+++ src/main/java/petshop/td/cliente/repository/ClienteRepository.java
@@ -12,7 +12,7 @@ public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 	@Query(value="select cliente.id_cliente, cliente.nome, contato_cliente.telefone from cliente join contato_cliente on cliente.id_cliente = contato_cliente.contato_cli join endereco_cliente on endereco_cliente.end_ref_cliente = contato_cliente.contato_cli", nativeQuery = true)
 	List<List> dados_cliente();
 	
-	//Mostra dados do cliente específico
+	//Mostra todos os dados de um cliente por ID
 	@Query(value="select cliente.id_cliente, cliente.nome, cliente.status_cliente, cliente.data_aniversario, cliente.observacao,cliente.genero, contato_cliente.telefone, contato_cliente.email, contato_cliente.rede_social, endereco_cliente.estado, endereco_cliente.cidade, endereco_cliente.bairro, endereco_cliente.logradouro, endereco_cliente.complemento,endereco_cliente.cep, pet.nome_pet, pet.tipo, pet.raca, pet.porte, pet.data_nascimento, pet.castrado, pet.sexo, pet.vacinado, pet.rede_social_pet, pet.observacao_pet from cliente join contato_cliente on cliente.id_cliente = contato_cliente.contato_cli join endereco_cliente on endereco_cliente.end_ref_cliente = contato_cliente.contato_cli join pet on pet.ref_id_cliente = endereco_cliente.end_ref_cliente where cliente.id_cliente = :id_cliente", nativeQuery = true)
 	List<List> cliente_id(Integer id_cliente);
 	
